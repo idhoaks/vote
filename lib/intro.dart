9930aa93da98ef7b219530926e1cfdad6ff22891
chiko <aryamarch087@gmail.com>
2020-09-24 17:10:46 +0700

    merah putih

--- lib/intro.dart
+++ lib/intro.dart
@@ -11,63 +11,76 @@ class _IntroState extends State<Intro> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              const Color.fromRGBO(255, 0, 0, 0.9),
-              const Color.fromRGBO(255, 255, 255, 0.9)
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                children: <Widget>[
+                  Container(
+                    color: Colors.red,
+                    height: MediaQuery.of(context).size.height / 2,
+                    width: MediaQuery.of(context).size.width / 1,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    height: MediaQuery.of(context).size.height / 2,
+                    width: MediaQuery.of(context).size.width / 1,
+                  ),
+                ],
+              ),
+              Center(
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+
+                  crossAxisAlignment: CrossAxisAlignment.center,
+
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 100),
+                      width: 300,
+                      height: 200,
+                      child: Image.asset(
+                        'img/v2024.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Container(
+                      width: 300,
+                      height: 200,
+                      child: Text(
+                        "Aplikasi ini adalah aplikasi polling dan survey tokoh-tokoh yang berpotensi dipilih dan terpilih menjadi Presiden dan Wakil Presiden Republik Indonesia 2024.",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontFamily: "Open Sans",
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MainScreen()));
+                      },
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 100, 0, 50),
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.35),
+                            borderRadius: BorderRadius.circular(200)),
+                        child: Icon(
+                          Icons.chevron_right,
+                          size: 35.0,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
-          )),
-          child: Center(
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 100),
-                  width: 300,
-                  height: 200,
-                  child: Image.asset(
-                    'img/v2024.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  width: 300,
-                  height: 200,
-                  child: Text(
-                    "Aplikasi ini adalah aplikasi polling dan survey tokoh-tokoh yang berpotensi dipilih dan terpilih menjadi Presiden dan Wakil Presiden Republik Indonesia 2024.",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontFamily: "Open Sans",
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MainScreen()));
-                  },
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 100, 0, 50),
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.35),
-                        borderRadius: BorderRadius.circular(200)),
-                    child: Icon(
-                      Icons.chevron_right,
-                      size: 35.0,
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ),
         ),
       ),
