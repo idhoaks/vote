56f70767295b9913608a7674179c061f46e9bd50
chiko <aryamarch087@gmail.com>
2020-09-24 15:25:55 +0700

    t1

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,169 +8,115 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int ukuranfont = 20;
+  List<String> items = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.fromLTRB(20, 50, 20, 0),
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.grid_on),
-                          Text("Icon 1"),
-                        ],
-                      ),
-                      // margin: EdgeInsets.only(top: 50),
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.red.withOpacity(0.5),
-                          // border: Border.all(color: Colors.black, width: 3),
-                          borderRadius: BorderRadius.circular(5)),
-                    ),
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.list),
-                          Text("Icon 1"),
-                        ],
-                      ),
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Colors.green.withOpacity(0.5),
-                          // border: Border.all(color: Colors.black, width: 3),
-                          borderRadius: BorderRadius.circular(5)),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 3, right: 3, top: 20),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  physics: ClampingScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      childAspectRatio: (400 / 600) // 3 ke kanan
-                      ),
-                  itemCount: 12, //jumlah item
-
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      margin: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                      decoration: BoxDecoration(
-                        // color: Colors.orange.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                          image: AssetImage("img/me.jpg"),
-                          fit: BoxFit.cover,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            Text("Top 5 Capres"),
+            Container(
+              height: 250,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      Container(
+                        height: 250,
+                        width: 150,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                      ),
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Single()));
-                        },
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                // height: 100,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(20),
-                                    topRight: Radius.circular(20),
-                                  ),
-                                  // color: Colors.red,
-                                  // image: DecorationImage(
-                                  //   image: AssetImage("img/me.jpg"),
-                                  // ),
-                                ),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color:
-                                                Colors.black.withOpacity(0.25),
-                                            blurRadius: 7,
-                                          )
-                                        ],
-                                      ),
-                                      height: 30,
-                                      width: MediaQuery.of(context).size.width,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        children: [
-                                          Text(
-                                            "86",
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              color: Colors.white70,
-                                            ),
-                                          ),
-                                          Icon(
-                                            Icons.star,
-                                            color: Colors.white70,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20),
-                                ),
-                                color: Colors.green,
-                              ),
-                              width: MediaQuery.of(context).size.width,
-                              height: 30,
-                              child: Center(
-                                child: Text(
-                                  "Nama",
-                                  style: TextStyle(
-                                    fontSize: ukuranfont.toDouble(),
-                                  ),
-                                ),
-                              ),
+                          children: <Widget>[
+                            Container(),
+                            Text(
+                              "Capres $index",
                             ),
                           ],
                         ),
                       ),
-                    );
-                  },
-                ),
+                      Positioned(
+                        bottom: 10,
+                        child: Container(
+                          height: 35,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+            Text("Top 5 Cawapres"),
+            Container(
+              height: 250,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      Container(
+                        height: 250,
+                        width: 150,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(),
+                            Text(
+                              "Capres $index",
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        child: Container(
+                          height: 35,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
